b4a8da442df321908e50c0450344ecbf737eb5f0
mirsafirst47 <habib.triande@gmail.com>
2020-08-05 17:30:52 -0400

    Can't sell my pets

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,69 @@
+require 'pry'
+
 class Owner
   # code goes here
+
+  @@all = []
+
+  attr_reader :name, :species
+  attr_accessor :cat, :dog
+
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all << self
+  end
+
+  def say_species
+    "I am a #{@species}."
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    self.all.count
+  end
+
+  def self.reset_all
+    self.all.clear
+  end
+
+  def cats
+    Cat.all.select{ |cat| cat.owner == self }
+  end
+
+  def dogs
+    Dog.all.select{ |dog| dog.owner == self }
+  end
+
+
+  def buy_cat(name)
+    Cat.new(name, self)
+  end
+
+  def buy_dog(name)
+    Dog.new(name, self)
+  end
+
+  def walk_dogs
+    Dog.all.each{ |dog| dog.mood = "happy" }
+  end
+
+  def feed_cats
+    Cat.all.each{ |cat| cat.mood = "happy" }
+  end
+
+  def sell_pets
+    all_pets = []
+    all_pets << Cat.all.select{ |cat| cat.owner == self }
+    all_pets << Dog.all.select{ |dog| dog.owner == self }
+
+    binding.pry
+  end
+
+  # binding.pry
+
 end
+
